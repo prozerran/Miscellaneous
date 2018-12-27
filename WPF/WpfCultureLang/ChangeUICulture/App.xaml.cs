331f0afcc5e8b53418ed0a9247d5418977011f97
prozerran <zaken520@gmail.com>
2018-12-27 17:45:11 +0800

    WPF folder changes

---
+++ WPF/WpfCultureLang/ChangeUICulture/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace ChangeUICulture
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        internal static void ChangeCulture(CultureInfo newCulture)
+        {
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+
+            var temp = Current.MainWindow;
+
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+
+            temp.Close();
+        }
+
+        // not sure what this func is for
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentUICulture.IetfLanguageTag)));
+        }
+    }
+}
---
+++ WPF/WpfCultureLang/ChangeUICulture/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace ChangeUICulture
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DateTime _displayDate = DateTime.Now;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateUIElements();
+        }
+
+        private void UpdateUIElements()
+        {
+            CurrentCultureTextBlock.Text = string.Format(
+                Properties.Resources.CurrentCulture,
+                CultureInfo.CurrentCulture.ToString());
+
+            DateTextBlock.Text = _displayDate.ToString("d");
+            LongDateTextBlock.Text = _displayDate.ToString("D");
+        }
+
+        private void USButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Properties.Resources.Culture = new CultureInfo("en-US");
+            App.ChangeCulture(new CultureInfo("en-US"));
+        }
+
+        private void CNButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Properties.Resources.Culture = new CultureInfo("zh-CN");
+            App.ChangeCulture(new CultureInfo("zh-CN"));
+        }
+
+        private void HantButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Properties.Resources.Culture = new CultureInfo("zh-Hant");
+            App.ChangeCulture(new CultureInfo("zh-Hant"));
+        }
+
+        private void HansButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Properties.Resources.Culture = new CultureInfo("zh-Hans");
+            App.ChangeCulture(new CultureInfo("zh-Hans"));
+        }
+
+        private void JAButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Properties.Resources.Culture = new CultureInfo("ja-JP");
+            App.ChangeCulture(new CultureInfo("ja-JP"));
+        }
+    }
+}

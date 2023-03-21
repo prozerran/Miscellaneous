10cfd3f4c58c688495f97381a52c9d63df915461
Tim <tim.hsu@10km.com>
2023-03-21 16:15:28 +0800

    core

---
+++ ConsoleCore/ConsoleCore/GreetingService.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+public class GreetingService : IGreetingService
+{
+    private readonly ILogger<GreetingService> _logger;
+    private readonly IConfiguration _config;
+
+    public GreetingService(ILogger<GreetingService> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public void Run()
+    {
+        for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+        {
+            // for logging purpose, use the following to grep
+            _logger.LogInformation("We are running on: {runNumber}", i);
+        }
+    }
+}
---
+++ ConsoleCore/ConsoleCore/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger.Information("Application Starting...");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) => 
+    { 
+        services.AddTransient<IGreetingService, GreetingService>();
+    })
+    .UseSerilog()
+    .Build();
+
+
+var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+svc.Run();
+
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"AppSettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables();
+}

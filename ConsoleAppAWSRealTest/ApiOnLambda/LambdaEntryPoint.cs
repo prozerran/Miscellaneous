7d39e2a70b94c061768d80c715ea4e11f2fbfd40
Yee Feng (Tim) Hsu <42185975+prozerran@users.noreply.github.com>
2023-05-22 14:39:08 +0800

    lambda test

---
+++ ConsoleAppAWSRealTest/ApiOnLambda/LambdaEntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace ApiOnLambda
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<StartupBase>();
+        }
+    }
+}
---
+++ ConsoleAppAWSRealTest/MinimalLambda/src/MinimalLambda/Controllers/FinalController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalLambda.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FinalController : Controller
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public FinalController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("getstring/{user}/{pass}")]
+        public IActionResult GetString(string user, string pass)
+        {
+            var str = string.Format($"The Name {user}, with pass {pass}!");
+            return Ok(str);
+        }
+    }
+}

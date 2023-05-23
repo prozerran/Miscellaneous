d7779ec4cca7a68cd368f185428e4bddb3aea53b
Yee Feng (Tim) Hsu <42185975+prozerran@users.noreply.github.com>
2023-05-23 10:40:53 +0800

    real lambda

---
+++ ConsoleAppAWSRealTest/RealLambda/src/RealLambda/Controllers/ActivityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//  dotnet lambda deploy-function
+
+namespace RealLambda.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivityController : Controller
+    {
+        private readonly ILogger<ActivityController> _logger;
+
+        public ActivityController(ILogger<ActivityController> logger)
+        {
+            _logger = logger;
+        }
+
+        // https://972oxcdpj9.execute-api.ap-southeast-1.amazonaws.com/Live/activity/test/5/9
+        [HttpGet("test/{x}/{y}")]
+        public IActionResult Test(int x, int y)
+        {
+            _logger.LogInformation($"{x} test {y} is {x + y}");
+
+            var str = string.Format($"Lets test {x} and {y}");
+            return Ok(str);
+        }
+    }
+}
